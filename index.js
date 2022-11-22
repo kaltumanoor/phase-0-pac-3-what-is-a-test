2d5bfd7a8ce138a9e8c9df292047fc2169759d28
kenhunja <garadcali@gmail.com>
2022-11-22 15:25:40 +0300

    complete with the assignments

--- index.js
+++ index.js
@@ -3,3 +3,40 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+const message = `${name} is ${height} inches tall`;
+console.log(message); 
+/*
+describe('what-is-a-test', () => {
+  describe('Name', () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual('Susan')
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () => {
+      expect(height).toBeLessThan(40)
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and height', () => {
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
+})
+*/
+In the first line, we're enabling the tests to access
+describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
